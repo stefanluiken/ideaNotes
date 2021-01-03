d1fd45d5f900e727489779d7d6eda16f99b46e2a
stefanluiken <stefanluiken@outlook.com>
2021-01-03 20:25:50 +0100

    Initial commit for ideaNotes

---
+++ app/src/main/java/com/example/ideanotes/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.ideanotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int requestCoreAddNote = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageAddNoteMain = findViewById(R.id.imageAddNoteMain);
+        imageAddNoteMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(
+                        new Intent(getApplicationContext(), CreateNoteActivity.class),
+                        requestCoreAddNote
+                );
+            }
+        });
+    }
+}
